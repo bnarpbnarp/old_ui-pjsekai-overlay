1bd109a94cb0dcd45e6916030c95d1d4ea251016
bnarpbnarp <118177032+bnarpbnarp@users.noreply.github.com>
2024-12-30 23:45:52 +0300

    changes

--- console.go
+++ console.go
@@ -11,17 +11,20 @@ import (
 func Title() {
 	fmt.Printf(
 		strings.TrimSpace(dedent.Dedent(`
-      %s== pjsekai-overlay -----------------------------------------------------------%s
-        %spjsekai-overlay / プロセカ風動画作成補助ツール%s
-        Version: %s%s%s
-        Developed by %s名無し｡(@sevenc-nanashi)%s
+      %s------------------------------- pjsekai-overlay -------------------------------%s
+        %sold pjsekai-overlay i think%s
+        Version: %s%s%s (based on version %s%s%s)
+        Original developed by %s名無し｡(@sevenc-nanashi)%s
+        old version fixed by %sbnarpbnarp%s
         https://github.com/sevenc-nanashi/pjsekai-overlay
       %s-------------------------------------------------------------------------------%s
-    `))+"\n\n",
+    `)) + "\n\n",
 		RgbColorEscape(0x00afc7), ResetEscape(),
 		RgbColorEscape(0x00afc7), ResetEscape(),
 		RgbColorEscape(0x0f6ea3), pjsekaioverlay.Version, ResetEscape(),
+                RgbColorEscape(0x614475), pjsekaioverlay.BaseVersion, ResetEscape(),
 		RgbColorEscape(0x48b0d5), ResetEscape(),
+                RgbColorEscape(0x5875a3), ResetEscape(),
 		RgbColorEscape(0xff5a91), ResetEscape(),
 	)
 
@@ -32,9 +35,10 @@ func RgbColorEscape(rgb int) string {
 }
 
 func AnsiColorEscape(color int) string {
-	return fmt.Sprintf("\033[38;5;%dm", color)
+  return fmt.Sprintf("\033[38;5;%dm", color)
 }
 
 func ResetEscape() string {
 	return "\033[0m"
 }
+
